5402adba3c6bdabcb27f6873e34a5a8de6f9af77
Adam Doherty <azdoherty91@gmail.com>
2019-01-13 16:08:42 -0500

    get data from street and city

--- js/script.js
+++ js/script.js
@@ -14,6 +14,14 @@ function loadData() {
     // load streetview
 	
     // YOUR CODE GOES HERE!
+	console.log("test post");
+	var street = "pre";
+	var city = "also pre";
+	console.log(street + ' ' +  city);
+	var street = $('#street').val();
+	var city = $('#city').val();
+	console.log(street + ' ' +  city);
+	
 
     return false;
 };
